bae2fe6a1e3e2b9e8fd4c76792df86ed1d4b0a13
ANdyUNLa <andy.dev.alnu@hotmail.com>
2020-04-26 23:22:06 -0300

    Cambios test

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -2,14 +2,34 @@ package com.company;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class App 
 {
     public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017")); // Puerto por defecto
-        MongoDatabase db = mongoClient.getDatabase("test");
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017")); // Puerto por defecto        
+        MongoDatabase db = mongoClient.getDatabase("prueba");
+        MongoCollection<Document> collection = db.getCollection("alumno");
+        Document doc = new Document("nombre", "Juan")
+                			.append("apellido", "Perez")
+                			.append("edad", 40);
+        collection.insertOne(doc);
         System.out.println("La base de datos ha sido cargada correctamente.");
     }
 }
