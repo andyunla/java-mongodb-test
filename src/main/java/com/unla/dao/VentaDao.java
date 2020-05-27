531e4d1637589e0ce86bc456ab0d3508b70b7b28
andyunla <49526728+andyunla@users.noreply.github.com>
2020-05-27 20:16:53 -0300

    VentaDao

---
+++ src/main/java/com/unla/dao/VentaDao.java
@@ -0,0 +1,94 @@
+package com.unla.dao;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.BSONObject;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import com.unla.datos.Venta;
+
+public class VentaDao {
+	private static VentaDao instancia = null;
+	private MongoDatabase db;
+	private MongoCollection<Document> collection;
+	private Gson gson;
+	
+	protected VentaDao() {
+		gson = new Gson();
+		db = MongoUtil.getDatabase();
+		collection = db.getCollection("ventas");
+	}
+	
+	public static VentaDao getInstance() {
+		if(instancia == null) {
+			instancia = new VentaDao();
+		}
+		return instancia;
+	}
+	
+	public Venta deserealizar(String json) {
+		return gson.fromJson(json, Venta.class);
+	}
+	
+	public Venta traer(String nroTicket) {
+		Venta venta = null;
+		String json = "{nroTicket: "+ nroTicket +"}";
+		BSONObject bson = (BSONObject)com.mongodb.util.JSON.parse(json);
+		FindIterable<Document> traidos = collection.find((Bson) bson);
+		if(traidos==null) {
+			System.out.println("No hay ningun venta con este nroTicket");
+		} else {
+			MongoCursor<Document> cursor = traidos.iterator();
+			venta = deserealizar(cursor.next().toJson());
+			cursor.close();
+		}
+		return venta;
+	}
+	
+	public List<Venta> traer() {
+		List<Venta> lista = new ArrayList<Venta>();
+		FindIterable<Document> fi = collection.find();
+        MongoCursor<Document> cursor = fi.iterator();
+        try {
+            while(cursor.hasNext()) {
+                lista.add(deserealizar(cursor.next().toJson()));
+            }
+        } finally {
+            cursor.close();
+        }
+		return lista;
+	}
+	
+	public void agregar(Venta objeto) {
+		String json = gson.toJson(objeto);
+		Document doc = Document.parse(json);
+		collection.insertOne(doc);
+	}
+	
+	public void modificar(Venta objetoModificado) {
+		BasicDBObject query = new BasicDBObject();
+		query.put("nroTicket", objetoModificado.getNroTicket());
+		
+		BasicDBObject newDocument = new BasicDBObject();
+		String json = gson.toJson(objetoModificado);
+		BSONObject bson = MongoUtil.jsonToBSONObject(json);
+		newDocument.putAll(bson);
+		BasicDBObject updateObject = new BasicDBObject();
+		updateObject.put("$set", newDocument);
+
+		collection.updateOne(query, updateObject);
+	}
+	
+	public long eliminar(Venta objeto) {
+		String json = "{nroTicket: '" + objeto.getNroTicket() + "'}";
+		BSONObject bson = (BSONObject)com.mongodb.util.JSON.parse(json);
+		DeleteResult result = collection.deleteOne((Bson)bson);
+		return result.getDeletedCount();
+	}
+}
